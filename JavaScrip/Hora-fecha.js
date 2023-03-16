4755bf86357026ac40b2a587c0eb6b9d1f75be1d
Sebastian <jsvelandia75@soy.sena.edu.co>
2023-03-15 21:14:14 -0500

    Archivo v3

--- JavaScrip/Hora-fecha.js
+++ JavaScrip/Hora-fecha.js
@@ -1,1 +1,25 @@
-<p id="date"></p>
+
+const canvas = document.getElementById("reloj");
+const ctx = canvas.getContext("2d");
+
+function drawText(time) {
+	const text = time.toLocaleTimeString();
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillText(text, 50, 50);
+}
+setInterval(() => {
+	const now = new Date();
+	drawText(now);
+}, 1000);
+
+var dialog = document.querySelector('dialog');
+dialogPolyfill.registerDialog(dialog);
+
+function showThisDialog(){
+   document.getElementById('this-dialog').show();
+
+}
+
+function hideThisDialog(){
+   document.getElementById('this-dialog').close();
+}
